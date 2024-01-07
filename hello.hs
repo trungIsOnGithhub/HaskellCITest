2f747dc1bcdf0d365561a2dc4eb0014b9132de35
trungvuive <trung150502@gmail.com>
2024-01-07 10:01:51 +0700

    add unit test

--- hello.hs
+++ hello.hs
@@ -1,2 +1,13 @@
-main =  do
-    putStrLn "Haskell Is Awesome"
+module HaskellExercise where
+
+addNum :: Int -> Int -> Int
+addNum num1 num2 = num1 + num2
+
+sumList :: Num a => [a] -> a
+sumList [] = 0
+sumList (x:xs) = x + sumList xs
+
+-- main :: IO()
+-- main =  do
+--    putStr "Sum of 2 numbers = "
+--    print(addNum 10 25)
