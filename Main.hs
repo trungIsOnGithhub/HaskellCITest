33dcf0fb9629d23269a5d7fae30904a7cbb2f319
trung <trung@123.com>
2024-01-17 10:11:20 +0700

    update test success case

--- Main.hs
+++ Main.hs
@@ -30,7 +30,7 @@ main = do
 
     print num1
 
-    let num2s = head test_lines
+    let num2s = head test_lines1
     let num2 = str_to_int num2s
     let test_lines2 = tail test_lines1
 
@@ -43,10 +43,10 @@ main = do
 
     if addNum num1 num2 /= num3 then do
         print "Test 1 failed"
-        -- die "One case has failed, Stopped!"
+        die "One case has failed, Stopped!"
     else print "Test 1 passed"
 
     if addNum 1 1 == 3 then do
         print "Test 2 failed"
-        -- die "One case has failed, Stopped!"
+        die "One case has failed, Stopped!"
     else print "Test 2 passed"
