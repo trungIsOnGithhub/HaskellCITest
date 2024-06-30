19ee17ed3f36fe756f44f4ebb2a56540c7f602d5
Trung Nguyen <trung.nguyen106@hcmut.edu.vn>
2024-06-30 21:51:25 +0700

    add new method support python 2

--- output/request.py
+++ output/request.py
@@ -1,4 +1,20 @@
 import os
+import urllib
+import urllib2
+
+def post_request(url, headers=None, data={}):
+    request = urllib2.Request(url, headers=headers or {})
+    data = urllib.urlencode(data).encode()
+    try:
+        with urllib2.urlopen(request, timeout=10, data=data) as response:
+            print(response.status)
+            return response.read(), response
+    except HTTPError as error:
+        print(error.status, error.reason)
+    except URLError as error:
+        print(error.reason)
+    except TimeoutError:
+        print("Request timed out")
 
 def send_text_output(submission_data : Dict):
     output_url = os.environ.get('OUTPUT_API_URI')
@@ -6,17 +22,7 @@ def send_text_output(submission_data : Dict):
     if output_url is None:
         raise Exception('Output URI is not provided')
 
-    try:
-        response = requests.post(output_url, json=submission_data)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except requests.exceptions.ConnectionError as conn_err:
-        print(f'Connection error occurred: {conn_err}')
-    except requests.exceptions.Timeout as timeout_err:
-        print(f'Timeout error occurred: {timeout_err}')
-    exce*Exception as err:
-        print(f'An error occurred: {err}')
+    post_request(output_url, data=submission_data)
 
 if __name__ == '__main__':
     send_text_output(sys.argv[0])
